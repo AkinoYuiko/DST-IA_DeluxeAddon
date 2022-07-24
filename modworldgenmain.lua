ac4ed590372a5ea2c1fb2a7edcad8eb64da1896b
Jerry <81011681+Jerry457@users.noreply.github.com>
2022-07-24 08:48:40 +0800

    fix volcano collapse

--- modworldgenmain.lua
+++ modworldgenmain.lua
@@ -1,9 +1,16 @@
+if not GetModConfigData("dragoonfly") then return end
+local AddLevelPreInit = AddLevelPreInit
 local StaticLayout = require("map/static_layout")
 local AllLayouts = require("map/layouts").Layouts
+GLOBAL.setfenv(1, GLOBAL)
 
 AllLayouts["VolcanoDragonflyArena"] = StaticLayout.Get("map/static_layouts/volcano_dragonfly")
-AllLayouts["VolcanoDragonflyArena"].ground_types = {GROUND.BRICK_GLOW}
+AllLayouts["VolcanoDragonflyArena"].ground_types = {WORLD_TILES.BRICK_GLOW}
 
-AddTaskSetPreInit("volcanoset", function(task_set_data)
-    task_set_data.set_pieces.VolcanoDragonflyArena = {count = 1, tasks = {"volcano"}}
+AddLevelPreInit("VOLCANO_LEVEL", function(level)
+    level.set_pieces = level.set_pieces or {}
+    level.require = level.require or {}
+
+    level.set_pieces.VolcanoDragonflyArena = {count = 1, tasks = {"Volcano"}}
+    table.insert(level.require, "dragonfly_spawner")
 end)
